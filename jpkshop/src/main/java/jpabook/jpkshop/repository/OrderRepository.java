852ac7928f5496755cc021de7897e87269e0bd53
clarent087 <d.artagnan087@gmail.com>
2021-09-19 23:22:56 +0900

    Update on 9/19
    웹계층 만들기

--- jpkshop/src/main/java/jpabook/jpkshop/repository/OrderRepository.java
+++ jpkshop/src/main/java/jpabook/jpkshop/repository/OrderRepository.java
@@ -1,10 +1,15 @@
 package jpabook.jpkshop.repository;
 
+import jpabook.jpkshop.domain.Member;
 import jpabook.jpkshop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -20,8 +25,83 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    // 동적 쿼리 - string 버전
+    public List<Order> findAllbyString(OrderSearch orderSearch) {
+        // 동적 쿼리 : orderSearch에 값이 있을수도 있고, 없을수도 있다.
+        // 동적 쿼리 핸들링은 지옥이라고 함.
+        // 단순히 name, status를 받는 query를 짜면 안됨. 값이 없을수도 있어서..
+        // mybatis는 동적쿼리를 할수있는게 xml로 잘되어 있다.
+        // 근데 jpa는...
+        
+        //language=JPAQL
+        String jpql = "select o From Order o join o.member m";
+        boolean isFirstCondition = true;
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " o.status = :status";
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
+            }
+            jpql += " m.name like :name";
+        }
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class).setMaxResults(1000); //최대 1000건
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
+        }
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query.getResultList();
+
+    }
+
+    // jpa Criteria로 해결하는 방법 ( jpql를 자바 코드로 생성하는 표준 방법)
+    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
+        Root<Order> o = cq.from(Order.class);
+        Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
+        List<Predicate> criteria = new ArrayList<>();
+        //주문 상태 검색
+        if (orderSearch.getOrderStatus() != null) {
+            Predicate status = cb.equal(o.get("status"),
+                    orderSearch.getOrderStatus());
+            criteria.add(status);
+        }
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            Predicate name =
+                    cb.like(m.<String>get("name"), "%" +
+                            orderSearch.getMemberName() + "%");
+            criteria.add(name);
+        }
+        cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
+        TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대 1000건
+        return query.getResultList();
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                        " where o.status = :status" +
+                        " and m.name like :name ", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus() ) // status 값이 안주어지면? sql에서 where랑 :status랑 뭘 비교?
+                .setParameter("name", orderSearch.getMemberName()) // name 값이 안주어지면?
+                .setFirstResult(100) // paging 코드 추가
+                .setMaxResults(1000)
+                .getResultList();
+
+    }
 
 }
