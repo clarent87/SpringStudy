a84f12fb33b01676e6a4c47fd3a0e2a2e260fa52
clarent <d.artagnan087@gmail.com>
2021-11-01 12:00:04 +0900

    Update on 11/01

--- servlet/src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
+++ servlet/src/main/java/hello/servlet/basic/response/ResponseHtmlServlet.java
@@ -1,14 +1,28 @@
 package hello.servlet.basic.response;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "responseHtmlServlet", urlPatterns = "/response-html")
 public class ResponseHtmlServlet extends HttpServlet {
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type: text/html;charset=utf-8
+            // html 보내는거라 type이 html
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println(" <div>안녕?</div>");
+        writer.println("</body>");
+        writer.println("</html>");
 
     }
 }
