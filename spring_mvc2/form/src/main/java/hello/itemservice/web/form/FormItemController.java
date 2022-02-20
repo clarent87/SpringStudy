04f226fbc3930897eb1f8b099b42d377581a2ce4
clarent <d.artagnan087@gmail.com>
2022-02-20 18:41:58 +0900

    메시지국제화 시작

--- spring_mvc2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ spring_mvc2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,24 +1,61 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
+@Slf4j
 public class FormItemController {
 
     private final ItemRepository itemRepository;
 
+    // ModelAttribute를 쓰면 regions key로 아래 내용이 담김.
+    // 즉 FormItemController의 어떤 method를 쓰던 model에 아래 내용이 default로 추가됨
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        // 주의. 성능최적화를 고민한다면, 아래 내용은 static영역에 만들어두고 가져와서 쓰는게 좋음
+        // 이거 controller의 메소드 호출시 마다 매번 생성되나봄... 안그러면..
+        // 근데 사실 이정도는 그렇게 성능에 영향이 있진않다고함.
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values(); // 이넘이 제공하는 api 변수들을 정의된 이넘들을 배열로 전달해줌
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+        return deliveryCodes;
+    }
+
+
     @GetMapping
     public String items(Model model) {
+
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "form/items";
@@ -34,11 +71,25 @@ public class FormItemController {
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
+
+        // 아래 코드는 여기 말고, editform 등등 모두 필요함. 이거 계속 쳐주기 불편
+        // 그래서 ModelAttribute 기능이 mvc에서 제공됨 ( 그 modelattribute 아님 )
+//        Map<String, String> regions = new LinkedHashMap<>(); // linkedHashMap을 써야지 값 넣은 순서가 보장됨
+//        regions.put("SEOUL", "서울");
+//        regions.put("BUSAN", "부산");
+//        regions.put("JEJU", "제주");
+//        model.addAttribute("regions", regions);
+
         return "form/addForm";
     }
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+
+        log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions()); // region 체크한거 배열로 잘들어옴
+        log.info("item.itemType={}", item.getItemType());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
