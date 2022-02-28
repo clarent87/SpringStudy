f32416b2c5b5772fd31cadf1068dd10875f0beb0
clarent <d.artagnan087@gmail.com>
2022-02-28 20:15:02 +0900

    쿠키 기본 소개

--- spring_mvc2/login/src/main/java/hello/login/web/HomeController.java
+++ spring_mvc2/login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,41 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
-    @GetMapping("/")
+    private final MemberRepository memberRepository;
+
+    //    @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    //Cookie는 httpservletRequest에서도 뽑을수 있음. 가장 쉽게 CookieValue를 이용함
+    // 쿠키값은 String인에 Long으로 스프링이 타입 컨버전해서 넣어줌. 해당 내용은 나중에 소개
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+
+        if (memberId == null) {
+            return "home";
+        }
+
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+        return "loginHome";
     }
 }
--- spring_mvc2/login/src/main/java/hello/login/web/login/LoginController.java
+++ spring_mvc2/login/src/main/java/hello/login/web/login/LoginController.java
@@ -2,7 +2,6 @@ package hello.login.web.login;
 
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -26,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
         }
@@ -38,9 +39,26 @@ public class LoginController {
             return "login/loginForm";
         }
 
-        // 로그인 성공 처리 TODO
+        // 로그인 성공 처리
+        // 쿠키에 시간 정보를 주지 않으면 세션 쿠키 (브라우저 종료시 모두 종료)
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId())); // 쿠키 값은 String을 넣어야 함
+        response.addCookie(idCookie);
 
         return "redirect:/";
+    }
 
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        // response 보낼때 HttpServletResponse말고 뭐있었지? ResponseEntity 같은것은 view resolver로 안들어감.
+
+        expireCookie(response, "memberId");
+
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0); // 이렇게 하면 쿠키 삭제됨. ( 아마 브라우저에서. )
+        response.addCookie(cookie);
     }
 }
