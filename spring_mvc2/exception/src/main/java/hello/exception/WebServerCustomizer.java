b143d912cb3bca8ac1d4ef79496284764e0f0b7f
clarent <d.artagnan087@gmail.com>
2022-03-03 23:04:40 +0900

    서블릿 예외 처리 - 작동원리
    까지 진행

---
+++ spring_mvc2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,27 @@
+package hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+// 서블릿 컨테이너가 하라는(제공하는) 대로 WebServerFactoryCustomizer<ConfigurableWebServerFactory> 를 이용함
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        // 404에러발생시 "/error-page/404" 컨트롤러를 호출해라! 란 코드
+        // ErrorPageController로 들어가게된다.
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+
+        // RuntimeException 발생시 "/error-page/500"로 가라!
+        // RuntimeException 예외의 자식 예외도 모두  "/error-page/500"로 보내줌
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        // 등록
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+    }
+}
