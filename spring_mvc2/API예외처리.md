ab59b75b24278a37b3c79e6475e012e18019c3c5
clarent <d.artagnan087@gmail.com>
2022-03-06 15:50:37 +0900

    예외처리와 오류 페이지 완료

---
+++ spring_mvc2/API예외처리.md
@@ -0,0 +1,19 @@
+# API 예외 처리
+
+## 시작
+
+## 스프링 부트 기본 오류 처리
+
+## HandlerExceptionResolver 시작
+
+## HandlerExceptionResolver 활용
+
+## 스프링이 제공하는 ExceprionResolver1
+
+## 스프링이 제공하는 ExceprionResolver2
+
+## @ExceptionHandler
+
+## @ControllerAdvice
+
+## 정리
--- spring_mvc2/exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ spring_mvc2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -6,7 +6,7 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 // 서블릿 컨테이너가 하라는(제공하는) 대로 WebServerFactoryCustomizer<ConfigurableWebServerFactory> 를 이용함
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
@@ -14,7 +14,7 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
     public void customize(ConfigurableWebServerFactory factory) {
         // 404에러발생시 "/error-page/404" 컨트롤러를 호출해라! 란 코드
         // ErrorPageController로 들어가게된다.
-        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND,  "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
 
         // RuntimeException 발생시 "/error-page/500"로 가라!
