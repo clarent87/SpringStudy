e1cacfde318dc0ac9df8c44910b14f183229bf1e
clarent <d.artagnan087@gmail.com>
2022-03-13 17:17:18 +0900

    포매터 - foramtter (end)

---
+++ spring_mvc2/typeconverter/src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
@@ -0,0 +1,27 @@
+package hello.typeconverter.formatter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.Formatter;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+@Slf4j
+public class MyNumberFormatter implements Formatter<Number> { // string formatter는 스프링이 기본으로 제공하니까 number를 예로 만듬
+
+    @Override
+    public Number parse(String text, Locale locale) throws ParseException {
+        log.info("text={}, locale={}", text, locale);
+        // "1,000" -> 1000
+        // 근데 위 기능 직접 구현하면 골치아픔. 그래서 java에서 미리 해당 기능을 제공함 그게 NumberFormat
+        NumberFormat format = NumberFormat.getNumberInstance(locale);
+        return format.parse(text);
+    }
+
+    @Override
+    public String print(Number object, Locale locale) {
+        log.info("object={}, locale={}", object, locale);
+        return NumberFormat.getNumberInstance(locale).format(object);
+    }
+}
