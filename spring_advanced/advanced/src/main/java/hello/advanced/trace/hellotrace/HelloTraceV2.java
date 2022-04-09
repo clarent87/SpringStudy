1735bff85f8127d4dd548f39edf1e445d2517f25
clarent <d.artagnan087@gmail.com>
2022-04-09 23:15:36 +0900

    예제만들기 완료

---
+++ spring_advanced/advanced/src/main/java/hello/advanced/trace/hellotrace/HelloTraceV2.java
@@ -0,0 +1,82 @@
+package hello.advanced.trace.hellotrace;
+
+import hello.advanced.trace.TraceId;
+import hello.advanced.trace.TraceStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class HelloTraceV2 {
+
+    private static final String START_PREFIX = "-->";
+    private static final String COMPLETE_PREFIX = "<--";
+    private static final String EX_PREFIX = "<X-";
+
+    private static String addSpace(String prefix, int level) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < level; i++) {
+            sb.append((i == level - 1) ? "|" + prefix : "|   ");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 로그 시작시 호출. TraceId 객체도 자동으로 생성.
+     *
+     * @param message
+     * @return TraceStatus : 이 객체를 나중에 end method로 전달 ( TraceId 및 message 저장됨 )
+     */
+    public TraceStatus begin(String message) {
+        TraceId traceId = new TraceId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
+        return new TraceStatus(traceId, startTimeMs, message);
+    }
+
+    //V2에서 추가
+    public TraceStatus beginSync(TraceId beforeTraceId, String message) {
+        TraceId nextId = beforeTraceId.createNextId();
+        Long startTimeMs = System.currentTimeMillis();
+        log.info("[" + nextId.getId() + "] " + addSpace(START_PREFIX, nextId.getLevel()) + message);
+        return new TraceStatus(nextId, startTimeMs, message);
+    }
+
+    /**
+     * 로그 종료시 호출
+     *
+     * @param status
+     */
+    public void end(TraceStatus status) {
+        complete(status, null);
+    }
+
+    /**
+     * 예외 발생시 이거를 호출 ( 종료 대신 )
+     *
+     * @param status
+     * @param e
+     */
+    public void exception(TraceStatus status, Exception e) {
+        complete(status, e);
+    }
+
+    /**
+     * end나 exception method에서 사용
+     * exception시는 param으로 e를 전달. 이걸로 log를 다른 형태로 출력해줌 ( 에러 정보 남기기 위해 )
+     *
+     * @param status
+     * @param e
+     */
+    private void complete(TraceStatus status, Exception e) {
+        Long stopTimeMs = System.currentTimeMillis();
+        long resultTimeMs = stopTimeMs - status.getStartTimeMs();
+        TraceId traceId = status.getTraceId();
+
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.getId(), addSpace(COMPLETE_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs);
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.getId(), addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs, e.toString());
+        }
+    }
+}
