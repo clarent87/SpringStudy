ae49cd213a6175cddda65f71bf72f1ba46d46ff9
clarent <d.artagnan087@gmail.com>
2022-04-15 01:45:03 +0900

    예제 프로젝트 만들기 v2 (완)

---
+++ spring_advanced/proxy/src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,28 @@
+package hello.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@RequestMapping // 역시 이것만 있어도 controller로 인식됨 . 여기서 Controller안쓴이유는  @Controller 는 @Component가 있어서 component scan되기 때문
+@ResponseBody
+public class OrderControllerV2 {
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
