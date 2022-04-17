194fbeac048297623866a3ed11e0210b524087c7
clarent <d.artagnan087@gmail.com>
2022-04-18 02:28:46 +0900

    JDK 동적 프록시 - 적용 2 (완)

--- spring_advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ spring_advanced/proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
+import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +10,9 @@ import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(InterfaceProxyConfig.class)
-@Import(ConcreteProxyConfig.class)
+//@Import(ConcreteProxyConfig.class)
+//@Import(DynamicProxyBasicConfig.class)
+@Import(DynamicProxyFilterConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의 -> AppConfig를 변경하며 쓰려고 만든거. 별다른 의미는 없음
 public class ProxyApplication {
 
@@ -24,6 +23,7 @@ public class ProxyApplication {
     /**
      * Todo: 뭐지 여기에 추가해도 bean 등록되나봄.
      * 보니까 SpringBootApplication 안에 @Configuration이 있음..
+     *
      * @return
      */
     @Bean
---
+++ spring_advanced/proxy/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceBasicHandler.java
@@ -0,0 +1,35 @@
+package hello.proxy.config.v2_dynamicproxy.handler;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class LogTraceBasicHandler implements InvocationHandler {
+    private final Object target;
+    private final LogTrace logTrace;
+
+    public LogTraceBasicHandler(Object target, LogTrace logTrace) {
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        TraceStatus status = null;
+        try {
+            // method.getDeclaringClass는 실제 method가 선언된 interface가 나옴.DeclaringClass 니까...
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
+            status = logTrace.begin(message);
+            //로직 호출
+            Object result = method.invoke(target, args);
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+
+    }
+}
